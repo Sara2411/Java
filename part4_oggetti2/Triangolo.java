45f0fb9a76605e4cd0162dadda77b49ac42d908f
Sara2411 <99330914+Sara2411@users.noreply.github.com>
2022-02-14 13:25:20 +0100

    Add files via upload

---
+++ part4_oggetti2/Triangolo.java
@@ -0,0 +1,48 @@
+public class Triangolo extends Forma
+{
+    
+    private double base;
+    private double altezza;
+    private double lato1;
+    private double lato2;
+    
+
+    public Triangolo(double base, double altezza, double lato1, double lato2)
+    {
+      this.base = base;
+      this.altezza = altezza;
+      this.lato1 = lato1;
+      this.lato2 = lato2;
+    }
+
+    public double getBase()
+    {
+      return base;
+    }
+
+    public double getAltezza()
+    {
+      return altezza;
+    }
+
+    public double getLato1()
+    {
+        return lato1;
+    }
+
+    public double getLato2()
+    {
+        return lato2;
+    }
+  
+    public double getPerimetro()
+    {
+        return base+lato1+lato2;
+    }
+
+    public double getArea()
+    {
+        return (base*altezza)/2;
+    }
+
+}
