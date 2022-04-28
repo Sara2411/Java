5bc450db56121d9176a5344687f6cfcd8d29ab0b
Sara2411 <sara.travaglini@accenture.com@domain.com>
2022-04-28 19:40:25 +0200

    Add files via upload

---
+++ part10_ricorsione/Palindroma.java
@@ -0,0 +1,13 @@
+public class Palindroma {
+
+    public boolean isPalindroma(String s) 
+    {
+        return isPalindroma(s, 0, s.length()-1);
+    }    
+
+    private boolean isPalindroma(String s, int a, int b) 
+    {
+        if (a>=b) return true;
+        return s.charAt(b) && isPalindroma(s, a+b, b-1);
+    }
+}
